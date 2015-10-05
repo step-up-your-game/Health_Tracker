add8f94cd3505452076cd5778ca93e41716d098e
Julie David <julie.angela.david@gmail.com>
2015-10-04 20:22:15 -0400

    Dashboard shows your exercises for the day.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,5 +4,6 @@ class DashboardController < ApplicationController
     @steps = Step.step
     @kilocals = Kilocal.kilocal
     @net = Kilocal.kilocal - (Step.step/25) - Exercise.burned
+    @exercise = Exercise.exercise
   end
 end
